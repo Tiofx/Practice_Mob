d63674930cbb4a9b63a893970a9a5fc37ba3d387
Tiofx <ioracle2718@gmail.com>
2017-07-03 18:03:58 +0300

    base RoomApi was created

--- app/src/main/java/com/example/st_pov/practice/models/models.kt
+++ app/src/main/java/com/example/st_pov/practice/models/models.kt
@@ -2,6 +2,7 @@ package com.example.st_pov.practice.models
 
 import android.media.Image
 
+//TODO: modify
 data class User(var email: String,
                 var password: String,
                 var fullName: String? = null)
@@ -10,6 +11,8 @@ data class Hotel(var title: String,
                  var starRating: Int,
                  var address: String? = null,
                  var photo: Image? = null)
+
+data class Room(var capacity: Int)
 
 data class Feedback(var feedback: String,
                     var rating: Int)
